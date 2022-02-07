5303970c862c81c05887ac0e8a4245cecac4d4c2
abhayagarwal5 <abhayag1998@gmail.com>
2022-02-07 23:38:52 +0530

    displayed show results and small fecth refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
-
+import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(response => response.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`search/shows?q=${input}`).then(result => {
+      // console.log(result);
+      setResults(result);
+    });
   };
   const onInputChange = event => {
     setInput(event.target.value);
@@ -17,6 +17,21 @@ const Home = () => {
     if (event.keyCode === 13) {
       onSearch();
     }
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item}>{item.show.name} </div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -30,6 +45,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
