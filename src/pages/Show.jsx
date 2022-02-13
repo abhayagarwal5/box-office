63a91b794536401eee08cff14bf94e310e7ef8c2
abhayagarwal5 <abhayag1998@gmail.com>
2022-02-13 12:13:18 +0530

    replaced useState with useReducer on the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,31 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS':
+      return { isLoading: false, error: null, show: action.show };
+    case 'FETCH_FAILURE':
+      return { ...prevState, isLoading: false, error: action.error };
+    default:
+      return prevState;
+  }
+};
+const initialState = {
+  isLoading: true,
+  show: null,
+  error: null,
+};
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [{ show, error, isLoading }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMount = true;
     apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`)
       .then(result => {
         if (isMount) {
-          setIsLoading(false);
-          setShow(result);
+          dispatch({ type: 'FETCH_SUCCESS', show: result });
         }
       })
       .catch(err => {
         if (isMount) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILURE', error: err.message });
         }
       });
     return () => {
       isMount = false;
     };
   }, [id]);
+  console.log(show);
 
   if (isLoading) {
     return <div>Data is being loaded</div>;
@@ -33,7 +51,6 @@ const Show = () => {
   if (error) {
     return <div>Error occured: {error}</div>;
   }
-  console.log(show);
   return <div>This is show page</div>;
 };
 
